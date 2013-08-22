ac0ab0f67990021877fc3cd07c3894dc664306f6
Josh Hug <gitjosh@joshh.ug>
2013-08-22 06:51:02 -0500

    added starter files

---
+++ Brogulate.java
@@ -0,0 +1,5 @@
+public class Brogulate {
+	public static void main(String[] args) {
+		System.out.println("Brogulators!");
+	}
+}
---
+++ DrawABee.java
@@ -0,0 +1,5 @@
+public class DrawABee {
+	public static void main(String[] args) {
+		System.out.println("a bee!");
+	}
+}
